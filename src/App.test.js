51a922a02c6f7a1423b1e86305878467f39e44a5
MarySalemme <marysalemme1@gmail.com>
2018-07-22 22:33:40 +0100

    Remove CRA material

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { shallow } from 'enzyme'
 
 it('renders without crashing', () => {
-  const wrapper = shallow(<App />)
-  const title = wrapper.find('.App-title')
-
-  expect(title.text()).toBe('Welcome to React')
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
