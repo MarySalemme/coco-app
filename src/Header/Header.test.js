e746b87e32fa57dc704b48c27c95e4ad5c995471
MarySalemme <marysalemme1@gmail.com>
2018-07-22 22:36:31 +0100

    Create header component

---
+++ src/Header/Header.test.js
@@ -0,0 +1,11 @@
+import Header from './Header'
+import React from 'react'
+import { shallow } from 'enzyme'
+
+test('Header component renders the header text', () => {
+  const wrapper = shallow(<Header />)
+
+  const name = wrapper.find('.name')
+
+  expect(name.text()).toBe('Coco Eventi')
+})
